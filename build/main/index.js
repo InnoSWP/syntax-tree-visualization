204a45fa8cecc05293b81eeda27ffb709d1667b1
alexandrachupkova <a.chupkova@innopolis.university>
2022-06-07 14:19:27 +0300

    add 1st draft of ui

--- build/main/index.js
+++ build/main/index.js
@@ -4,17 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+//import JavaScript from "tree-sitter-javascript"
+//import Parser from "tree-sitter"
+//let parser = new Parser()
+//parser.setLanguage(JavaScript)
 let app = (0, express_1.default)();
+let port = 8080;
 // just for test
-app.get('/users', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.writeHead(200, {
         'Content-type': 'text/html;'
     });
-    res.write('<h1 style="color: red; text-align: center">Hello Nikolay!!!</h1><img src="http://localhost:8080/cow.gif">');
+    res.write(fs_1.default.readFileSync(path_1.default.join(__dirname, '..', 'resources', 'templates', 'index.html')));
     res.end();
-    console.log('Get request on /users');
+    console.log('Get request on /');
 });
 app.use(express_1.default.static('src/resources'));
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
