06e0afe6ad608db3cf68f79b6ca03f788994e44f
anton <SkorkinAnton@yandex.ru>
2022-06-07 13:10:57 +0300

    test index.html

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,18 +1,26 @@
 import express from "express"
+import fs from "fs"
+import path from "path"
+//import JavaScript from "tree-sitter-javascript"
+//import Parser from "tree-sitter"
 
+//let parser = new Parser()
+//parser.setLanguage(JavaScript)
 let app = express()
 
+let port = 8080
+
 // just for test
-app.get('/users', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.writeHead(200, {
         'Content-type': 'text/html;'
     })
-    res.write('<h1 style="color: red; text-align: center">Hello Nikolay!!!</h1><img src="http://localhost:8080/cow.gif">')
+    res.write(fs.readFileSync(path.join(__dirname, '..', 'resources', 'templates', 'index.html')))
     res.end()
-    console.log('Get request on /users')
+    console.log('Get request on /')
 })
 app.use(express.static('src/resources'))
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080")
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
 })
---
+++ src/main/service/SyntaxTreeService.ts
@@ -0,0 +1,11 @@
+// import JavaScript from "tree-sitter-javascript"
+// import Parser from "tree-sitter"
+//
+// let parser = new Parser()
+// parser.setLanguage(JavaScript)
+
+// class SyntaxTreeService {
+//     public Tree function getTreeFrom(code: string) {
+//
+//     }
+// }
