6c7797270e77688265246c54d362e49bdff7a638
Nikita Fedorovich <95264734+Uzifam@users.noreply.github.com>
2022-06-22 20:59:03 +0300

    Add files via upload

---
+++ build/main/service/saveService.ts
@@ -0,0 +1,61 @@
+const mysql = require("mysql2");
+
+
+function hashCode(string: String) {
+    var hash = 0, i, chr;
+    if (string.length === 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        chr = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "swpbase",
+    password: "12345"
+});
+
+
+// const sql = `create table if not exists base(
+//   id int primary key auto_increment,
+//   code varchar(5000) not null,
+//   hash varchar(1000) not null
+// )`;
+
+function saveService(Line: String) {
+    let Hash = hashCode(Line);
+    const sql = `INSERT INTO base(code, hash)
+                 VALUES ?`;
+    var data = [[Line, Hash]];
+    connection.query(sql, [data], function (err: String, results: String) {
+    });
+
+    connection.end();
+    return Hash;
+}
+
+function returnCode(Hash: String) {
+    let ans = "-1";
+
+    const sql = "SELECT * FROM base";
+    connection.query(sql, function (err: String, results: String) {
+        if (err) console.log(err);
+        const users = results;
+        for (let i = 0; i < users.length; i++) {
+            // @ts-ignore
+            if (users[i].hash == Hash) {
+                // @ts-ignore
+                ans = (users[i].code);
+            }
+        }
+
+        connection.end();
+    });
+
+    return ans;
+}
