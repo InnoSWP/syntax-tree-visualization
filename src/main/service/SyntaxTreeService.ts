1e534c33366edb4a53a95552d29f970cbe27aaa6
levpen <levpen@yandex.ru>
2022-06-26 16:34:38 +0300

    Added badTypes and fixed array tails

--- src/main/service/SyntaxTreeService.ts
+++ src/main/service/SyntaxTreeService.ts
@@ -6,11 +6,30 @@ let parser = new Parser()
 parser.setLanguage(JavaScript)
 
 export class SyntaxTreeService {
-    #ind = 0;
-    #code = ""
+    #ind:number = 0;
+    #code:string = ""
     #tree = undefined
     #array = undefined
+    #badTypes:string[] = [
+        "variable_declarator",
+        ";",
+        "(",
+        ")",
+        "{",
+        "}",
+        "function",
+        "return",
+        "statement_block",
+        "return_statement",
+        "formal_parameters",
+        "arguments"
+    ];
 
+    addBadType(type:string) {
+        this.#badTypes.push(type);
+    }
+
+    
     getTreeFrom(code: string) {
         if (code == this.#code)
             return this.#tree
@@ -25,10 +44,12 @@ export class SyntaxTreeService {
         return this.#array
     }
 
+    
     #generate(code: string) {
         let tree = parser.parse(code);
         let node = tree.rootNode
         let ans = this.#getTree(node)
+
         this.#code = code
         // @ts-ignore
         this.#tree = ans.tree
@@ -42,6 +63,7 @@ export class SyntaxTreeService {
 
         this.#dfsTree(node, arr, tree);
         this.#fillZeroes(arr);
+        this.#tailElimination(arr);
 
         return {tree, arr};
     }
@@ -53,31 +75,64 @@ export class SyntaxTreeService {
                 arr[i].cur_arr.push(0);
         }
     }
-    #dfsTree(node: Parser.SyntaxNode, arr: any, tree: any, depth = 0) {
-        if (node == null || node.isMissing())
-            return;
-        tree.type = node.type;
-        tree.text = node.text;
-        tree.position = {start: node.startPosition, end: node.endPosition};
-        tree.children = [];
-        arr.push({cur_arr:[], text:node.text, type:node.type});
+    #tailElimination(arr:any){
+        let len = arr.length-1;
+        for(let i = len; i > 0; --i){
+            for(let j = arr[i].cur_arr.length-1; j > 0; --j){
+                if(arr[i-1].cur_arr[j] == 0)
+                    return;
+                if(arr[i].cur_arr[j] == arr[i-1].cur_arr[j])
+                    arr[i].cur_arr[j] = 0;
+                else
+                    break;
+            }
+        }
+    }
+    #addArray(node: Parser.SyntaxNode, arr: any, depth:number) {
         let i = 0, prevCount = (this.#ind > 0)? arr[this.#ind - 1].cur_arr.length: 0;
+        arr.push({cur_arr:[], text:node.text, type:node.type});
 
         while (i < Math.max(depth + 1, prevCount)) {
             if (i < prevCount)
                 arr[this.#ind].cur_arr.push(arr[this.#ind - 1].cur_arr[i]);
             else
                 arr[this.#ind].cur_arr[i] = 0;
-            if (depth == i)
+            if (depth == i){
                 arr[this.#ind].cur_arr[i]++;
+            }
             ++i;
         }
 
+    }
+    #dfsTree(node: Parser.SyntaxNode, arr: any, tree: any, depth:number = 0, childCount = 0) {
+        if (node == null || node.isMissing())
+            return;
+        
+        if(!this.#badTypes.includes(node.type)){
+            tree.type = node.type;
+            tree.text = node.text;
+            tree.position = {start: node.startPosition, end: node.endPosition};
+            tree.children = [];
+        }
+
+        if(!this.#badTypes.includes(node.type))
+            this.#addArray(node, arr, depth);
+
         for (let i = 0; i < node.childCount; ++i) {
-            ++this.#ind;
-            tree.children.push({});
+            
             // @ts-ignore
-            this.#dfsTree(node.child(i), arr, tree.children[i], depth + 1);
+            if(!this.#badTypes.includes(node.child(i).type)){
+                ++this.#ind;
+                tree.children.push({});
+                // @ts-ignore
+                this.#dfsTree(node.child(i), arr, tree.children[childCount++], depth + 1);
+            }
+            else{
+                // tree.children.push({});
+                // @ts-ignore
+                this.#dfsTree(node.child(i), arr, tree, depth, childCount);
+                childCount = tree.children.length;
+            }
         }
     }
 }
