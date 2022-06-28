2752ca07dfc711c8e74a43778d500f5af87aa503
alexandrachupkova <a.chupkova@innopolis.university>
2022-06-28 20:47:30 +0300

    add: code refactoring

---
+++ src/resources/static/js/help-window.js
@@ -0,0 +1,11 @@
+var helpWindow = document.getElementById("help");
+var btn = document.getElementById("help-button");
+let spanHelp = document.getElementById("helpclose");
+btn.onclick = function () {
+    helpWindow.style.display = "block";
+}
+
+spanHelp.onclick = function () {
+    helpWindow.style.display = "none";
+
+}
---
+++ src/resources/static/js/share-window.js
@@ -0,0 +1,11 @@
+var shareWindow = document.getElementById("share");
+var btn = document.getElementById("share-button");
+let spanShare = document.getElementById("shareclose");
+
+btn.onclick = function () {
+    shareWindow.style.display = "block";
+}
+
+spanShare.onclick = function () {
+    shareWindow.style.display = "none";
+}
