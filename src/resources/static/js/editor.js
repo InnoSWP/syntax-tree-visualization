d860c2aadd79f331e05d6dcfe209b22da74e4f53
Anton <SkorkinAnton@yandex.ru>
2022-07-03 00:36:54 +0300

    Add: saving files, updating url, refactoring repo code

--- src/resources/static/js/editor.js
+++ src/resources/static/js/editor.js
@@ -1,4 +1,5 @@
 import {generateTree} from "./tree-builder.js";
+import {building} from "./building-array.js";
 
 // @ts-ignore
 export let editor = ace.edit("editor");
@@ -6,20 +7,23 @@ let script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 editor.setTheme("ace/theme/xcode");
 editor.session.setMode("ace/mode/javascript");
+editor.setValue(document.getElementById("default_text").innerText)
+generateTree(document.getElementById("default_text").innerText) // штука работает не совсем корректно
+building(document.getElementById("default_text").innerText) // штука работает не совсем корректно
 editor.session.on('change', function (/** @type {any} */ _delta) {
-  console.log(getvalue());
-  let jsonData = { code: editor.getValue() };
-  $.ajax({
-    url: '/tree',
-    type: 'POST',
-    data: jsonData,
-    success: function (data) {
-      generateTree(data);
-    }
-  })
+    console.log(getvalue());
+    let jsonData = {code: editor.getValue()};
+    $.ajax({
+        url: '/tree',
+        type: 'POST',
+        data: jsonData,
+        success: function (data) {
+            generateTree(data);
+        }
+    })
 });
 
 function getvalue() {
-  return JSON.stringify(editor.getValue(''));
+    return JSON.stringify(editor.getValue(''));
 }
 
--- src/resources/static/js/share-window.js
+++ src/resources/static/js/share-window.js
@@ -1,11 +1,22 @@
 var shareWindow = document.getElementById("share");
 var button = document.getElementById("share-button");
 let spanShare = document.getElementById("shareclose");
+import {editor} from "../js/editor.js"
 
 button.onclick = function () {
-  shareWindow.style.display = "block";
+    shareWindow.style.display = "block";
+    $.ajax({
+        url: '/save',
+        type: 'POST',
+        data: {code: editor.getValue('')},
+        success: function (data) {
+            console.log(data);
+            window.history.pushState({}, null, "/?hash=" + data)
+            // Прикрутите пж чтобы url показывался на share страничке, не только в строке наверху
+        }
+    })
 }
 
 spanShare.onclick = function () {
-  shareWindow.style.display = "none";
+    shareWindow.style.display = "none";
 }
