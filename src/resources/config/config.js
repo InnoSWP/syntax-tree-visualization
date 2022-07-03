334fc82335d9c7b583340b0d8707cd16f0a7d322
Anton <SkorkinAnton@yandex.ru>
2022-07-03 13:27:52 +0300

    Remove credentials

--- src/resources/config/config.js
+++ src/resources/config/config.js
@@ -1,8 +1,8 @@
-export let config = {
+export const config = {
     "mysql": {
-        "host": process.env["MYSQL_HOST"],
-        "user": process.env["MYSQL_USER"],
-        "database": process.env["MYSQL_DATABASE"],
-        "password": process.env["MYSQL_PASSWORD"]
+        "host": process.env.MYSQL_HOST,
+        "user": process.env.MYSQL_USER,
+        "database": process.env.MYSQL_DATABASE,
+        "password": process.env.MYSQL_PASSWORD
     }
 }
