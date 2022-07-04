e6b63966c870fb25299f20a883ded4231887202a
Anton <SkorkinAnton@yandex.ru>
2022-07-04 10:01:14 +0300

    inti

--- src/resources/static/js/share-window.js
+++ src/resources/static/js/share-window.js
@@ -1,15 +1,23 @@
 var shareWindow = document.getElementById("share");
 var button = document.getElementById("share-button");
 let spanShare = document.getElementById("shareclose");
+import {editor} from "../js/editor.js"
 
-// @ts-ignore
 button.onclick = function () {
-  // @ts-ignore
-  shareWindow.style.display = "block";
+    shareWindow.style.display = "block";
+    $.ajax({
+        url: '/save',
+        type: 'POST',
+        data: {code: editor.getValue('')},
+        success: function (data) {
+            console.log(data);
+            window.history.pushState({}, null, "/?hash=" + data)
+            document.getElementById('URL-share').value = window.location.href;
+            // Прикрутите пж чтобы url показывался на share страничке, не только в строке наверху
+        }
+    })
 }
 
-// @ts-ignore
 spanShare.onclick = function () {
-  // @ts-ignore
-  shareWindow.style.display = "none";
+    shareWindow.style.display = "none";
 }
